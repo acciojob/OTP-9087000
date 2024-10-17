cad460f22ae364aaf9ca8accd88599c3606f203b
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-17 16:39:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
-//your JS code here. If required.
-const codeContainer = document.querySelector(".code-container");
-const numberOfInputs = 8;
-let lastEnteredInputElement = null;
-
-
-for (let i = 1; i <= numberOfInputs; i++) {
-    const input = document.createElement("input");
-    input.className = "code";
-    input.maxLength = 1;
-
-    input.addEventListener("input", onInput);
-    input.addEventListener("keyup", onKeyUp);
-	input.addEventListener("keypress", onBackSpace);
-	input.addEventListener("keyup", onEnter);
-
-    codeContainer.appendChild(input);
-}
-
-function onInput(event) {
-    const currentElement = event.target;
-    console.log("event triggered");
-    if (currentElement.value) {
-        lastEnteredInputElement = currentElement;
-        currentElement.nextElementSibling && currentElement.nextElementSibling.focus();
-    }
-}
-
-function onBackSpace(e) {
-    e.target.value = "";
-    e.target.previousElementSibling && e.target.previousElementSibling.focus();
-}
-
-function onEnter(e) {
-    if (e.target.value) {
-        e.target.nextElementSibling && e.target.nextElementSibling.focus();
-    }
-}
-
-function onKeyUp(e) {
-    if (e.key === "Enter") {
-        onEnter(e);
-    }
-    else if (e.key === "Backspace") {
-        onBackSpace(e);
-    }
-}
+const codes = document.querySelectorAll(".code")
+ 
+codes[0].focus()
+ 
+codes.forEach((code,idx)=>{
+    code.addEventListener("keydown",(e)=>{
+        console.log(e.key)
+        const keyPressed = e.key
+ 
+        if(keyPressed>=0 && keyPressed<=9){
+            codes[idx].value=''
+            if(idx<codes.length-1){
+                setTimeout(()=>codes[idx+1].focus(),10)
+                // setTimeout(function(){
+                //     codes[index+1].focus()
+                // },10)
+            }
+        }
+        else if(keyPressed==="Backspace"){
+            if(idx>0){
+                setTimeout(()=>codes[idx-1].focus(),10)
+            }
+        }
+    })
+})
